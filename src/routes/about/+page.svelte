0be1f53cd851937a3ba4cf64e18864ae62e01057
Brent Danley <brentdanley@gmail.com>
2022-11-19 19:32:55 -0500

    Adds post-content styles

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,4 +2,4 @@
 	let name = 'Brent';
 </script>
 
-<h1>Hi, I'm {name}!</h1>
+<h2>Hi, I'm {name}!</h2>
---
+++ src/routes/blog/[slug]/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '$lib/styles/post-content.scss';
+</script>
+
+<div class="post-content">
+	<h4 class="blog-title">This is a blog post.</h4>
+	<slot />
+</div>

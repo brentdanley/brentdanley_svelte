d7f300a2d4ac186cea897ba5ec18d620e1816913
Brent Danley <brentdanley@gmail.com>
2023-05-19 21:09:59 -0400

    Add content to About page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,8 +2,39 @@
 	let name = 'Brent';
 </script>
 
-<h1>About</h1>
+<h1>About Me</h1>
 
-<p>I'll put something on this page eventually...</p>
+<p>
+	Hello, and welcome to my digital abode. I'm {name}, a zealous explorer of the universe we inhabit,
+	both through the lens of science and the thrill of adventure.
+</p>
 
-<p>Definitely check out my <a href="https://brentdanley.codes">coding</a> site.</p>
+<p>
+	My curiosity is kindled by a profound love for science, especially in areas that unmask the
+	secrets of life and the cosmos. My heart beats a little faster for biology, where I find the
+	intricate symphony of life forms both humbling and awe-inspiring. The boundless expanse of space
+	as mirrored in the roar and rush of rockets has always ignited my imagination. But my fascination
+	doesn't stop there. Emerging technologies and their profound potential for transforming our future
+	have always captured my attention, with the theory of evolution being a cornerstone in my
+	scientific exploration.
+</p>
+
+<p>
+	But life, for me, isn't just about theories and hypotheses. I relish the thrill of physical
+	adventure. From the swift agility of tennis to the exhilarating trails of mountain biking, from
+	the calming strokes of kayaking to the magnificent views from the cockpit of my airplane, I savor
+	the taste of adrenaline.
+</p>
+
+<p>
+	When I'm not out on an adventure or immersed in scientific pondering, you'll find me in my loft
+	indulging in the art of brewing the perfect cup of latte, each pour a testament to my love for the
+	process. And, just as the sun ushers in a new day, I savor a crème brûlée, because as they say -
+	YOLO!
+</p>
+
+<p>
+	This balance between my passion for science, the rush of sports, the serenity of nature, and the
+	simple pleasures in life is what defines me. As you journey through my site, I invite you to share
+	in these experiences and discoveries, to join me on this exciting ride. Welcome aboard!
+</p>

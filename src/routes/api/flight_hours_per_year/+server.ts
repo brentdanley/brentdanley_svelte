2bcbe126e30564ecec587195fd81a5aeb8178cbd
Brent Danley <brentdanley@gmail.com>
2023-09-24 19:28:39 -0400

    WIP

--- src/routes/api/flight_hours_per_year/+server.ts
+++ src/routes/api/flight_hours_per_year/+server.ts
@@ -13,44 +13,58 @@ interface LogbookResponse {
 }
 
 export async function GET(): Promise<LogbookResponse> {
-	// Read JSON file
-	const filePath = path.resolve('/static/data/pilot_logbook.json');
-	const rawData = fs.readFileSync(filePath, 'utf8');
-	const data = JSON.parse(rawData);
+	try {
+		// Read JSON file
+		const filePath = path.resolve('./static/data/pilot_logbook.json');
 
-	const groupedDataObject = data.reduce((acc: any, cur: any) => {
-		const year = cur.date.split('-')[0];
-		const flightHours = parseFloat(cur.flight_hours); // Make sure it's a number
+		const rawData = fs.readFileSync(filePath, 'utf8');
+		const data = JSON.parse(rawData);
 
-		if (acc[year]) {
-			acc[year].total_hours += flightHours;
-		} else {
-			acc[year] = {
-				year,
-				total_hours: flightHours
-			};
-		}
-		return acc;
-	}, {});
+		const groupedDataObject = data.reduce((acc: any, cur: any) => {
+			const year = cur.date.split('-')[0];
+			const flightHours = parseFloat(cur.flight_hours); // Make sure it's a number
 
-	// Round total_hours to 1 decimal place for each year
-	Object.keys(groupedDataObject).forEach((year) => {
-		groupedDataObject[year].total_hours = parseFloat(
-			groupedDataObject[year].total_hours.toFixed(1)
-		);
-	});
-
-	// Sort years and create a sorted array
-	const years = Object.keys(groupedDataObject).sort();
-	const groupedDataArray = years.map((year) => groupedDataObject[year]);
-
-	return new Response(
-		JSON.stringify({
-			status: 200,
-			body: groupedDataArray,
-			headers: {
-				'content-type': 'application/json'
+			if (acc[year]) {
+				acc[year].total_hours += flightHours;
+			} else {
+				acc[year] = {
+					year,
+					total_hours: flightHours
+				};
 			}
-		})
-	);
+			return acc;
+		}, {});
+
+		// Round total_hours to 1 decimal place for each year
+		Object.keys(groupedDataObject).forEach((year) => {
+			groupedDataObject[year].total_hours = parseFloat(
+				groupedDataObject[year].total_hours.toFixed(1)
+			);
+		});
+
+		// Sort years and create a sorted array
+		const years = Object.keys(groupedDataObject).sort();
+		const groupedDataArray = years.map((year) => groupedDataObject[year]);
+
+		return new Response(
+			JSON.stringify({
+				status: 200,
+				body: groupedDataArray,
+				headers: {
+					'content-type': 'application/json'
+				}
+			})
+		);
+	} catch (err) {
+		console.log(err);
+		return new Response(
+			JSON.stringify({
+				status: 500,
+				body: { message: 'Internal Server Error' },
+				headers: {
+					'content-type': 'application/json'
+				}
+			})
+		);
+	}
 }

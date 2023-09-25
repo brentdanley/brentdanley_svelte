71efd927288c395ab73d34ee36f453aeac328bc0
Brent Danley <brentdanley@gmail.com>
2023-09-24 20:45:18 -0400

    Adds copy to logbook page

--- src/routes/pilot-logbook/+page.svelte
+++ src/routes/pilot-logbook/+page.svelte
@@ -4,4 +4,16 @@
 
 <h1>Pilot Logbook</h1>
 
+<p>
+	I earned my private pilot certificate way back on October 7, 1998. My first kid would be born less
+	than a year later, followed by our second in 2001 and third in 2003. Between raising kids, going
+	to school, moving to Maine, and then having my world dumped upside-down when my wife died in 2016,
+	flying was often not a priority. I thought about it a lot, though; blue skies always beckoned me
+	to return to them.
+</p>
+<p>
+	In 2021 I accepted a position at ForeFlight, after which I returned to flight with a vengeance!
+</p>
+<p>I love how my logbook data not only tells the story of my flying, but also of my life.</p>
+
 <FlightHoursChart />

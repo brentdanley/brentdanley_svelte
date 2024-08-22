9b89d3d0cbe6e3b24cfba7ca5d598f0d523f6ecd
Brent Danley <brentdanley@gmail.com>
2024-08-22 11:22:40 -0400

    Creates ai bread post.

--- src/lib/components/CloudinaryImage.svelte
+++ src/lib/components/CloudinaryImage.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
 	export let alt: string;
-	export let image_name: string;
+	export let public_id: string;
 	export let caption: string;
+
+	console.log('image name', public_id);
 </script>
 
 <div class="cloudinary-image">
 	<img
-		srcset={`https://res.cloudinary.com/brentdanley/image/upload/w_480/v1532733435/brentdanley.com/${image_name}.webp 480w, 
-        https://res.cloudinary.com/brentdanley/image/upload/w_800/v1532733435/brentdanley.com/${image_name}.webp 800w, 
-        https://res.cloudinary.com/brentdanley/image/upload/w_1400/v1532733435/brentdanley.com/${image_name}.webp 1400w`}
+		srcset={`https://res.cloudinary.com/brentdanley/image/upload/w_480/v1532733435/${public_id}.webp 480w, 
+        https://res.cloudinary.com/brentdanley/image/upload/w_800/v1532733435/${public_id}.webp 800w, 
+        https://res.cloudinary.com/brentdanley/image/upload/w_1400/v1532733435/${public_id}.webp 1400w`}
 		sizes="(max-width: 1400px) 80vw, 1400px"
-		src={`https://res.cloudinary.com/brentdanley/image/upload/w_600/v1532733435/brentdanley.com/${image_name}.webp`}
+		src={`https://res.cloudinary.com/brentdanley/image/upload/w_600/v1532733435/${public_id}.webp`}
 		{alt}
 	/>
 	{#if caption != undefined}
---
+++ src/routes/blog/baking-bread-with-ai.md
@@ -0,0 +1,17 @@
+---
+title: 'Baking Break with AI'
+date: '2024-08-01'
+tags: [ai, bread, baking, cooking]
+---
+
+<script>
+    import CloudinaryImage from '$lib/components/CloudinaryImage.svelte'
+</script>
+
+<CloudinaryImage public_id="brentdanley.com/bread_ai/ai_bread_hero" alt="Baking bread with AI blog hero image" />
+
+<CloudinaryImage public_id="brentdanley.com/bread_ai/bread_cookbook" caption="bread with ai" alt="cookbook bread recipe" />
+
+## Baking bread with AI
+
+So, I've baked this bread loaf recipe several times. I know how to do it. I rarely want the nice, big cookbook out while I'm baking. I don't want it to get stained or wet, it takes up too much precious counter space, and frankly the recipe is far too detailed for what I need. When I first baked this bread, the lengthy descriptions were helpful. Now, all I need are the ingredients and the basic instructions.
